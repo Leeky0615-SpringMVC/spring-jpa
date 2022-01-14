175f4889e5fc4343795caf237689cef6bf4d9da0
Leeky0615 <rbdus7174@naver.com>
2022-01-14 19:18:22 +0900

    feat : learn jpa & spring data jpa(web expansion)

--- src/main/java/study/springjpa/dto/MemberDto.java
+++ src/main/java/study/springjpa/dto/MemberDto.java
@@ -3,6 +3,7 @@ package study.springjpa.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import study.springjpa.entity.Member;
 
 @Data
 @AllArgsConstructor
@@ -11,4 +12,9 @@ public class MemberDto {
     private Long id;
     private String username;
     private String teamName;
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+    }
 }
